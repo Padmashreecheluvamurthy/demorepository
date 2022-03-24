27fa7b9f1a99fcfcca8532fc1d25cbfae446f52c
Lenovo <Lenovo@DESKTOP-PG39M5C>
2022-03-24 12:17:43 +0530

    user 1 first commit

---
+++ src/test/java/com/crm/demoproject2/demo1/CreateContactTest.java
@@ -0,0 +1,14 @@
+package com.crm.demoproject2.demo1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createContact()
+	{
+		System.out.println("user -1  contact created");
+		
+	}
+	
+
+}
